c1858d8fcf6c3f1fc0607dd9767eca681f51cf36
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2021-06-23 12:24:42 +0300

    Добавлена базовая ViewModel и ViewModel для окна приложения. +Добавлена привязка нового slider к знаечению угла Elipse и TextBox, которые показываеют значения слайдеров по вертикали и горизонтали.

---
+++ Test/MailSender.TestWPF/ViewModels/WpfMailSenderViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using MailSender.TestWPF.ViewModels.Base;
+
+namespace MailSender.TestWPF.ViewModels
+{
+	public class WpfMailSenderViewModel : ViewModel
+	{
+		private string title = "Главное окно приложения!";
+
+		/// <summary>
+		/// Заголовок окна
+		/// </summary>
+		public string Title
+		{
+			get => title;
+			set => _ = Set(ref title, value);
+		}
+	}
+}
