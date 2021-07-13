45a21b82434ee5f0646e448df5d1f010aad88ec0
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2021-07-13 20:55:53 +0300

    Репозиторий серверов

---
+++ MailSender/Services/InMemory/InMemoryServerRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MailSender.Models;
+
+namespace MailSender.Services.InMemory
+{
+	public class InMemoryServerRepository : InMemoryRepository<Server>
+	{
+		private static IEnumerable<Server> GetTestData(int count = 10) => Enumerable.Range(1, count)
+			.Select(i => new Server
+			{
+				Id = i,
+				Name = $"Сервер {i}",
+				Address = $"smtp.server-{i}.ru",
+				Login = $"User-{i}",
+				Password = $"Password - {i}",
+				UseSSL = i % 2 == 0
+			});
+
+		public InMemoryServerRepository() : base(GetTestData()) { }
+
+		public override void Update(Server item)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
