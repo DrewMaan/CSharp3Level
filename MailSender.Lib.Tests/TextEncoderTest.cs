9b9d25c8addd93f702dc51df9a2e10b463320e08
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2021-07-15 12:16:08 +0300

    Добавление TestEncoder и модульных тестов к нему

---
+++ MailSender.Lib.Tests/TextEncoderTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailSender.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSender.Lib.Tests
+{
+	[TestClass]
+	public class TextEncoderTest
+	{
+		[TestMethod]
+		public void Encode_ABC_return_BCD_with_key_default()
+		{
+			//AAA
+
+			//Arange - подготовка данных
+			var pass = "ABC";
+			var expected = "BCD";
+
+			//Act - действие, нацеленное на тестирование кода
+			var actual = TextEncoder.Encode(pass);
+
+			//Assert - проверка утверждений
+			Assert.AreEqual(expected, actual);
+
+			//StringAssert.Matches();
+			//CollectionAssert.AreEquivalent();
+		}
+
+		[TestMethod]
+		public void Decode_BCD_return_ABC_with_key_default()
+		{
+			//AAA
+
+			//Arange - подготовка данных
+			var pass = "BCD";
+			var expected = "ABC";
+
+			//Act - действие, нацеленное на тестирование кода
+			var actual = TextEncoder.Decode(pass);
+
+			//Assert - проверка утверждений
+			Assert.AreEqual(expected, actual);
+
+			//StringAssert.Matches();
+			//CollectionAssert.AreEquivalent();
+		}
+	}
+}
---
+++ MailSender.Lib/Services/TextEncoder.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace MailSender.Services
+{
+	public class TextEncoder
+	{
+		public static string Encode(string pass, int key = 1) => new(pass.Select(c => (char) (c + key)).ToArray());
+
+		public static string Decode(string pass, int key = 1) => new(pass.Select(c => (char) (c - key)).ToArray());
+	}
+}
