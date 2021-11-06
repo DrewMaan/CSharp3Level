bcb866331497be4fd7780273b0d8f90ce3568d64
Andrey <kasimovskiy.a.d.public@gmail.com>
2021-11-06 20:03:54 +0300

    Паралелльное чтение файлов, вычисление результата и запись в файл.

---
+++ CalculateNumbersFromFiles/FileGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace CalculateNumbersFromFiles
+{
+	public static class FileGenerator
+	{
+		static Random rand = new Random();
+		public static string FilesPath = @"..\..\..\Files\";
+
+		public static void GenerateFiles(int numbers = 1)
+		{
+			for (int i = 0; i < numbers; i++)
+			{
+				using var fstream = new FileStream(FilesPath + "file" + i + ".dat", FileMode.OpenOrCreate);
+				var numbers1 = rand.NextDouble() * 10;
+				var numbers2 = rand.NextDouble() * 10;
+				byte[] input = Encoding.Default.GetBytes($"{rand.Next(1, 3)} {numbers1.ToString("F6")} {numbers2.ToString("F6")}");
+
+				fstream.Write(input);
+			}
+		}
+	}
+}
---
+++ CalculateNumbersFromFiles/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateNumbersFromFiles
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FileGenerator.GenerateFiles(10000);
+			File.WriteAllText(FileGenerator.FilesPath + "result.dat", string.Empty);
+			var filesNames = Directory.GetFiles(FileGenerator.FilesPath).Where(x => x != FileGenerator.FilesPath + "result.dat").ToArray();
+			using var fstreamResult = new FileStream(FileGenerator.FilesPath + "result.dat", FileMode.Append);
+			object fstreamResultLock = new object();
+
+			Parallel.ForEach(filesNames, (fileName) =>
+			{
+				double result = 0.0;
+
+				using var sr = new StreamReader(fileName);
+				string[] arguments = sr.ReadLine().Split(' ');
+
+				if(int.Parse(arguments[0]) == 1)
+					result = double.Parse(arguments[1]) * double.Parse(arguments[2]);
+				else
+					result = double.Parse(arguments[1]) / double.Parse(arguments[2]);
+
+				byte[] input = Encoding.Default.GetBytes(result.ToString() + "\n");
+				lock(fstreamResultLock)
+				{
+					fstreamResult.Write(input);
+				}
+			});
+		}
+	}
+}
