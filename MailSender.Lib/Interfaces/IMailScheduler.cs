af0a221dae613df5a862bae032490aa0fc3ae878
Andrey <kasimovskiy.a.d.public@gmail.com>
2022-01-18 21:45:58 +0300

    Интерфейс планировщика задач

---
+++ MailSender.Lib/Interfaces/IMailScheduler.cs
@@ -0,0 +1,20 @@
+﻿using MailSender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Interfaces
+{
+	public interface IMailScheduler
+	{
+		void Start();
+		void Stop();
+		SchedulerTask AddToPlan(
+			DateTime time,
+			Consignor consignor,
+			EmailsList emails,
+			Server server);
+	}
+}
